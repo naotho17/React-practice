f7c206bbd584e290bc8b0f706ee114efdd70b1ab
naotho17 <77154584+naotho17@users.noreply.github.com>
2021-10-16 11:06:42 +0900

    How to use "useEffect"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("さいしょ");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag); // 「!」は逆という意味
+  };
+
+  //useEffectの第二引数([])には、最初の一回だけ通したいような処理を実行することが出来る
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //eslint-disable-next-line
+  }, [num]); //numに関心を持たせる
+
   return (
     <>
       {/* こっちは直接スタイルを書き込んでいる */}
@@ -13,7 +32,10 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(￣∀￣)</p>}
     </>
   );
 };
